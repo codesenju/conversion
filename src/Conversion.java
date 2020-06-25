5636e910cb3b9a36cb967a6cf8bb4917c1631568
codesenju <codesenju.gmail.com>
2020-06-25 20:13:42 +0200

    Code Update

--- src/Conversion.java
+++ src/Conversion.java
@@ -3,16 +3,16 @@ import java.io.IOException;
 
 public class Conversion {
 
-    public float kelToCel(float kelvin) throws NumberFormatException, IOException {
+    public double kelToCel(double kelvin) throws NumberFormatException, IOException {
 
-        return kelvin - 273.15F;
+        return kelvin - 263.15D;
     }
 
-    public double MilesToKm(double miles) {
-        return miles * 1.60934;
+    public double milesToKm(double miles) {
+        return miles * 1.60934D;
     }
 
-    public double LbsToKg(double pounds) {
-        return pounds * 0.4536;
+    public double lbsToKg(double pounds) {
+        return pounds * 0.4536D;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        final double ten = 10D;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Kelvin: ");
+        double kelvin = in.nextDouble();
 
-
-       
+        Conversion c = new Conversion();
+        System.out.println("Kel to Cel: " + c.kelToCel(kelvin));
+        System.out.println("Lbs to KG: " + c.lbsToKg(ten));
+        System.out.println("Miles to KM: " + c.milesToKm(ten));
     }
 }
