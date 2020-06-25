96dbb50228a553f67920c3c30b90aedbedf6af0b
codesenju <codesenju.gmail.com>
2020-06-25 17:41:32 +0200

    Added milesToKm & lbsToKg methods in the conversion class

--- src/Conversion.java
+++ src/Conversion.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 public class Conversion {
 
     public float kelToCel(float kelvin) throws NumberFormatException, IOException {
-    // Kelvin to Degree Celsius Conversion
-    float celsius = kelvin - 273.15F;
-    return celsius;
-}
 
+        return kelvin - 273.15F;
+    }
 
+    public double MilesToKm(double miles) {
+        return miles * 1.60934;
+    }
+
+    public double LbsToKg(double pounds) {
+        return pounds * 0.4536;
+    }
 }
