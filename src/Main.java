a5fcaea783bce5ec4db287d4ea053bcccf3f3f40
codesenju <codesenju.gmail.com>
2020-06-25 20:51:12 +0200

    added do while loop

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
-        final double ten = 10D;
+        // final double ten = 10D;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter Kelvin: ");
-        double kelvin = in.nextDouble();
-
+        double userInput = 0;
+        String answer = "y";
         Conversion c = new Conversion();
-        System.out.println("Kel to Cel: " + c.kelToCel(kelvin));
-        System.out.println("Lbs to KG: " + c.lbsToKg(ten));
-        System.out.println("Miles to KM: " + c.milesToKm(ten));
+
+        System.out.println("Menu");
+        System.out.println("---");
+        System.out.println("a) Kelvin to Celsius");
+        System.out.println("b) Pounds to Kilograms");
+        System.out.println("c) Miles to Kilometres");
+        System.out.println("");
+        
+        do {
+            System.out.print("enter a, b or c : ");
+            String menuOption = in.next();
+
+            if (menuOption.toLowerCase().contains("a")) {
+
+                System.out.print("Enter Kelvin: ");
+                userInput = in.nextDouble();
+                System.out.println(userInput + " Kelvin = " + c.kelToCel(userInput) + " Celsius");
+
+            } else if (menuOption.toLowerCase().contains("b")) {
+                System.out.print("Enter Pounds: ");
+                userInput = in.nextDouble();
+                System.out.println(userInput + " lbs = " + c.lbsToKg(userInput) + " kg");
+
+            } else if (menuOption.toLowerCase().contains("c")) {
+                System.out.print("Enter Miles: ");
+                userInput = in.nextDouble();
+                System.out.println(userInput + " Miles = " + c.milesToKm(userInput) + " km");
+            }
+
+            System.out.println("Do you want to go back to Main Menu (y/n)? ");
+            answer = in.next();
+
+        } while (answer.toLowerCase().contains("y"));
+
+        in.close();
+
     }
 }
