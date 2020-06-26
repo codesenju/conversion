88b36351e85c35c766d3fa8c9bf844049fdb4e5b
codesenju <codesenju@gmail.com>
2020-06-26 12:21:48 +0200

    commented out mvn commands

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Prerequisites: - maven & docker
-mvn compile && mvn package
+#mvn compile && mvn package
 docker build -t codesenju/conversion .
 docker run --rm -ti codesenju/conversion
