eb89f3391d5dd17ac9ebb4e779ae18589d3ac067
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-07-24 09:47:22 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Prerequisites: - maven & docker
 # Testing github webhook trigger
+# test 2
 #mvn compile && mvn package
 docker build -t codesenju/conversion .
 docker run --rm -ti codesenju/conversion
